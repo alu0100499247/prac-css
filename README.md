836e3e6321865a9acee6237f116c2e7240a3c9b3
Cristhian Garcia <alu0100499247@ull.edu.es>
2013-10-12 13:59:15 +0100

    Versión Inicial

---
+++ README.md
@@ -0,0 +1,60 @@
+Práctica 4 - STW
+==============
+
+Objetivo
+---------
+
+En la práctica anterior ya introducimos **Haml** para crear un template del cuál se extrae el código para mostrar los resultados de la práctica.
+
+En ésta intentaremos añadir hojas de estilo para darle mas color y vistosidad a la página donde mostraremos los resultados
+
+Contenido
+---------
+
+El contenido del fichero que se presenta es el siguiente:
+
+* Gemfile
+* Gemfile.lock
+* README
+* Rakefile
+* lib
+	* ppt.rb
+* public
+	* css
+	* img
+	* js
+* ppt.rb
+* views
+	* index.haml
+
+
+Ejecución	
+---------
+
+Podemos ejecutar la práctica de las siguientes formas:
+
+* Como en el directorio hemos específicado un Rakefile, podemos introducir en la consola el siguiente comando: `rake`. Luego con un navegador iremos a la página `localhost:9292` para visualizar el resultado.
+
+* También podemos introducir por consola el siguiente comando: `ruby ppt.rb`. Luego con un navegador iremos a la página `localhost:9292` para visualizar el resultado.
+
+
+#####Asignatura: Sistemas y Tecnologías Web.
+
+|  Alumno |  Alu  |
+|---------|-------|
+|  Cristhian Javier García Conrado  |  alu0100499247  |
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ ppt.rb
@@ -0,0 +1,65 @@
+require 'rack/request'
+require 'rack/response'
+require 'haml'
+
+
+module PiedraPapelTijeras
+	class App
+		def initialize(app=nil)
+			@app = app
+			@content_type = :html
+			@defeat = {'piedra' => 'tijeras', 'papel' => 'piedra', 'tijeras' => 'papel'}
+			@throws = @defeat.keys
+			$choose = @throws.map { |x|
+				%Q{ <li><a href="/?choice=#{x}"> #{x} </a></li> }
+			}.join("\n")
+			$choose = "<p>\n<ul>\n#{$choose}\n</ul>"
+		end
+
+		def call(env)
+			req = Rack::Request.new(env)
+
+			req.env.keys.sort.each { |x| puts "#{x} => #{req.env[x]}" }
+			
+			computer_throw = @throws.sample
+			player_throw = req.GET["choice"]
+			answer = if !@throws.include?(player_throw)
+				"Elige una de las siguientes opciones:"
+			elsif player_throw == computer_throw
+				"Has empatado! Elige de nuevo :)"
+			elsif computer_throw == @defeat[player_throw]
+				"Bien! #{player_throw} gana a #{computer_throw}!! :D"
+			else
+				"Ohh! :( #{computer_throw} gana a #{player_throw}! Suerte en la próxima! ;)"
+			end
+
+			engine = Haml::Engine.new File.open("views/index.haml").read
+			res = Rack::Response.new
+			res.write engine.render({},
+				:answer => answer,
+				:choose => $choose,
+				:throws => @throws)
+			res.finish
+		end
+	end
+end
+
+if $0 == __FILE__
+	require 'rack'
+	require 'rack/showexceptions'
+	Rack::Server.start(
+		:app => Rack::ShowExceptions.new(
+					Rack::Lint.new(
+						PiedraPapelTijeras::App.new)),
+		:Port => 9292,
+		:server => 'thin'
+	)
+end
+
+
+
+
+
+
+
+
